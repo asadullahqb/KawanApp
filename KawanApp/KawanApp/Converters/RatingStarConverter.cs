e828e426f26acca68a384482408252f632b0b460
Asad <asadqb16@gmail.com>
2020-01-18 21:28:52 +0800

    70% done View All Profiles page Need to do: - refine function - formatting for the message response time (this is a descriptive analytics)

---
+++ KawanApp/KawanApp/Converters/RatingStarConverter.cs
@@ -0,0 +1,42 @@
+﻿using KawanApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using Xamarin.Forms;
+
+namespace KawanApp.Converters
+{
+    public class RatingStarConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int startype;
+            if (value is int)
+            {
+                var converter = new ImageSourceConverter();
+                startype = (int)value;
+
+                switch (startype)
+                {
+                    case 0:
+                        return (ImageSource)converter.ConvertFromInvariantString("starEmpty.png");
+                    case 1:
+                        return (ImageSource)converter.ConvertFromInvariantString("starHalf.png");
+                    case 2:
+                        return (ImageSource)converter.ConvertFromInvariantString("starFilled.png");
+                }
+
+                return null;
+            }
+            else
+                return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- KawanApp/KawanApp/Models/KawanUser.cs
+++ KawanApp/KawanApp/Models/KawanUser.cs
@@ -7,6 +7,77 @@ namespace KawanApp.Models
     public class KawanUser : User
     {
         public double Rating { get; set; }
+        public int[] Stars 
+        { 
+            //0 is for empty star, 1 is for half star, 2 is for filled star. s[n] refers to star position n+1
+            get 
+            {
+                int[] s = new int[5]; //This array stores the code for each star.
+
+                if (Rating == 5)
+                {
+                    s[4] = 2; s[3] = 2; s[2] = 2; s[1] = 2; s[0] = 2; //Fill all stars
+                }
+                else if (Rating == 4)
+                {
+                    s[3] = 2; s[2] = 2; s[1] = 2; s[0] = 2; //Fill first 4 stars
+                }
+                else if (Rating == 3)
+                {
+                    s[2] = 2; s[1] = 2; s[0] = 2; //Fill first 3 stars
+                }
+                else if (Rating == 2)
+                {
+                    s[1] = 2; s[0] = 2; //Fill first 2 stars
+                }
+                else if (Rating == 1)
+                {
+                    s[0] = 2; //Fill first star
+                }
+                else if ((Rating - 4) > 0)
+                    if (((Rating - 4) * 10) >= 5)
+                    {
+                        s[4] = 1; s[3] = 2; s[2] = 2; s[1] = 2; s[0] = 2; //Fill half of last star and fill all other stars
+                    }
+                    else
+                    {
+                        s[4] = 0; s[3] = 2; s[2] = 2; s[1] = 2; s[0] = 2; //Leave last star blank and fill all other stars
+                    }
+                else if ((Rating - 3) > 0)
+                    if (((Rating - 3) * 10) >= 5)
+                    {
+                        s[3] = 1; s[2] = 2; s[1] = 2; s[0] = 2; //Fill half of second last star and fill all stars before it
+                    }
+                    else
+                    {
+                        s[3] = 0; s[2] = 2; s[1] = 2; s[0] = 2; //Leave second last star blank and fill all other stars before it
+                    }
+                else if ((Rating - 2) > 0)
+                    if (((Rating - 2) * 10) >= 5)
+                    {
+                        s[2] = 1; s[1] = 2; s[0] = 2; //Fill half of middle star and fill all other stars before it
+                    }
+                    else
+                    {
+                        s[2] = 0; s[1] = 2; s[0] = 2; //Leave middle star blank and fill all other stars before it
+                    }
+                else if ((Rating - 1) > 0)
+                    if (((Rating - 1) * 10) >= 5)
+                    {
+                        s[1] = 1; s[0] = 2; //Fill half second star and fill first star
+                    }
+                    else
+                    {
+                        s[1] = 0; s[0] = 2; //Leave second star blank and fill first star
+                    }
+                else if ((Rating) > 0)
+                    if ((Rating * 10) >= 5)
+                        s[0] = 1; //Fill half of first star
+                    else
+                        s[0] = 0; //Leave all stars blank
+                return s; 
+            } 
+        }
         public string AverageResponseTime { get; set; }
     }
 }
