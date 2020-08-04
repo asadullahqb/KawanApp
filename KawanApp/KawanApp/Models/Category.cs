721948dbfa71fe796cbbfc2c5e59ea709e3afcf3
adam-fikri <61441425+adam-fikri@users.noreply.github.com>
2020-08-04 15:05:49 +0800

    Create Category.cs

---
+++ KawanApp/KawanApp/Models/Category.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KawanApp.Models
+{
+    public class Category
+    {
+        public int Cat_id { get; set; }
+        public string Cat_name { get; set; }
+        public string Cat_description { get; set; }
+    }
+}
