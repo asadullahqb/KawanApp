ca9d9a4ce83d6e5a8d558fdd7e2a95a8b39b963e
MDaimNazmi <62739571+MDaimNazmi@users.noreply.github.com>
2020-08-05 11:30:58 +0800

    Create Report.cs

---
+++ KawanApp/KawanApp/Models/Report.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KawanApp.Models
+{
+    public class Report
+    {
+        public int Report_id { get; set; }
+        public string Report_content { get; set; }
+        public User User { get; set; }
+    }
+}
